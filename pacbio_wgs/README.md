4cd8232296390d949e4acdf0ef32dcd1587b9356
Tychele Turner <tycheleturner@gmail.com>
2023-03-18 14:11:56 -0500

    [hifiasm] initial commit of hifiasm scripts

--- pacbio_wgs/README.md
+++ pacbio_wgs/README.md
@@ -10,7 +10,10 @@ Date: March 18, 2023
 bsub -R 'span[hosts=1] rusage[mem=310G]' -g /tychele/dv -n 31 -M 310G -q tychele -G compute-tychele -oo HT22_hicanu.oo -a "docker(tychelewustl/canu:2.2)" sh running_hicanu.sh
 ```
 
-
+## Hifiasm Assembly on PacBio Reads only
+### Server submssion (run on WashU RIS LSF server, basic submission components listed below)```
+bsub -R 'span[hosts=1] rusage[mem=2700G]' -g /tychele/dv -n 62 -M 2700G -q tychele -G compute-tychele -oo ht22_hifiasm.oo -a "docker(tychelewustl/hifiasm:version2)" sh running_hifiasm_ht22.sh
+```
 
 
 
---
+++ pacbio_wgs/running_hifiasm_ht22.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Tychele N. Turner, Ph.D.
+# Last update: March 18, 2023
+# Hifiasm analysis workflow
+
+export PATH=/opt/conda/bin:$PATH
+
+#SAMPLE="$1"
+#FASTQ="$2"
+
+#hifiasm -o "$SAMPLE".asm -t31 "$FASTQ"
+
+hifiasm -o HT22.hifiasm.asm -t31 /path/to/SMRTcell1/CCS_Data/m64136_230127_224537.hifi_reads.fastq.gz /path/to/SMRTcell2/CCS_Data/m64136_230130_173712.hifi_reads.fastq.gz /path/to/SMRTcell3/CCS_Data/m64136_230201_043221.hifi_reads.fastq.gz /path/to/SMRTcell4/CCS_Data/m64136_230205_053720.hifi_reads.fastq.gz /path/to/SMRTcell5/CCS_Data/m64136_230207_183356.hifi_reads.fastq.gz /path/to/SMRTcell6/CCS_Data/m64136_230209_052845.hifi_reads.fastq.gz
+
+
