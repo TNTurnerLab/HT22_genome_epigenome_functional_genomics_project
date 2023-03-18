08995e2a1b0e57f8ad96a0757cb885efa9b33ba1
Tychele Turner <tycheleturner@gmail.com>
2023-03-18 14:07:44 -0500

    [script] add HiCanu script

---
+++ pacbio_wgs/running_hicanu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=/opt/conda/bin:$PATH
+
+SAMPLE='HT22'
+
+canu -p "$SAMPLE"_hicanu_assembly -d "$SAMPLE"_hicanu_assembly maxThreads=31 maxMemory=300 genomeSize=3g useGrid=false -pacbio-hifi /path/to/SMRTcell1/CCS_Data/m64136_230127_224537.hifi_reads.fastq.gz /path/to/SMRTcell2/CCS_Data/m64136_230130_173712.hifi_reads.fastq.gz /path/to/SMRTcell3/CCS_Data/m64136_230201_043221.hifi_reads.fastq.gz /path/to/SMRTcell4/CCS_Data/m64136_230205_053720.hifi_reads.fastq.gz /path/to/SMRTcell5/CCS_Data/m64136_230207_183356.hifi_reads.fastq.gz /path/to/SMRTcell6/CCS_Data/m64136_230209_052845.hifi_reads.fastq.gz
+
